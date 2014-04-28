d2a2385026d176144ece0e99e50ed05e890c58a1
Lan Xu <snowery.xu@gmail.com>
2014-04-27 22:16:20 -0700

    output predict result in sample format

--- mf/MatrixFactorization.py
+++ mf/MatrixFactorization.py
@@ -7,10 +7,20 @@ from operator import add, mul
 
 class mf:
     def __init__(self, file_path, train_file, test_file, real_file=None):
+        """
+        file_path: the folder where data files reside
+        train_file: (user, item, rating) quote records
+        test_file: (user, item) records, preferences to be predicted
+        real_file: (user, option, value) real purchase records, can be none if it doesn't exist
+        For this specific project:
+        item here is the combination of options with their values,
+            e.g. item 10 denotes option A with choice 0; item 21 denotes option B with choice 1
+        rating is the number of quotes for a certain item by a user
+        """
         self.file_path = file_path
         config = SparkConf().setMaster("local").setAppName("Kaggle")\
             .set("spark.executor.memory", "2g")\
-            .set("spark.storage.memoryFraction", "0.5")
+            .set("spark.storage.memoryFraction", "1")
 
         sc = SparkContext(conf=config)
 
@@ -25,35 +35,47 @@ class mf:
                 .map(lambda line: [float(x) for x in line.split(',')]).map(lambda r: ((r[0], r[1]), r[2]))
 
     def train(self,  rank=3, iterations=20, lambda_=0.01, alpha=0.01, blocks=-1):
+        """
+        train a mf model against the given parameters
+        """
         model = ALS.trainImplicit(self.train_data, rank, iterations, lambda_, blocks, alpha)
         return model
 
     def predict(self, model):
+        """
+        predict each customer's preference towards each item,
+        and keep the most preferable choice for each option as the result
+        """
         predictions = model.predictAll(self.test_data)\
             .map(lambda r: ((r[0], int(r[1])/10), (int(r[1])%10, r[2]))).reduceByKey(lambda a, b: a if a[1] > b[1] else b)
 
-        predictions.saveAsTextFile("file:" + self.file_path)
         return predictions
 
     def evaluate(self, pred, output):
-        # Evaluate the model on training data
-        #testdata = ratings.map(lambda p: (int(p[0]), int(p[1])))
-
+        """
+        evaluate the prediction against the purchase data from two perspectives:
+        1: for each option A, B, ..., G, calculate the accuracy
+        2: for each customer, evaluate the accuracy of the predicted (A,B,..,G) combination
+        """
         real_pred = self.real_data.join(pred)
-
-        real_pred.saveAsTextFile("file:" + self.file_path)
-
         eval_by_option = real_pred.map(lambda row: (row[0][1], row[1][0] == row[1][1][0])).reduceByKey(add)
-
         eval_by_cid = real_pred.map(lambda row: (row[0][0], row[1][0] == row[1][1][0]))\
             .reduceByKey(mul)
 
         correct = eval_by_cid.map(lambda row: row[1]).reduce(add)
         total = self.real_data.count()/7
         # evaluate accuracy for each option [A:G]
-
         for e in eval_by_option.collect():
             output.write(chr(ord('A') + int(e[0]) - 1) + " total: {0}, correct: {1}, accuracy: {2}%\n".format(total, e[1], e[1]*100.0/total))
 
         output.write("Aggregate all options: total: {0}, correct: {1}, accuracy: {2}%\n".format(total, correct, correct*100.0/total))
 
+    def output_predict(self, pred):
+        """
+        save to file in the sample format(cid, 1111111)
+        """
+        # map to (cid, (option, 'value'))
+        outputs = pred.map(lambda r: (int(r[0][0]), (int(r[0][1]), str(int(r[1][0])))))
+        # map to (cid, 'values')
+        outputs = outputs.groupByKey().map(lambda x: (x[0], "".join(zip(*sorted(x[1], key=lambda pair: pair[0]))[1])))
+        outputs.sortByKey().map(lambda x: ",".join([str(x[0]), x[1]])).saveAsTextFile("file:" + self.file_path)
--- mf/practice.py
+++ mf/practice.py
@@ -2,19 +2,21 @@ __author__ = 'lan'
 from MatrixFactorization import mf
 import csv
 
-file_path = "/Users/lan/documents/sjsu/239/project/"
+file_path = "/Users/lan/documents/sjsu/239/project/dataset/"
 parameters = open(file_path + "parameters.csv", 'Ur')
 reader = csv.reader(parameters, delimiter=',')
 result = open(file_path + "results", 'a')
 
-nmf = mf(file_path, "train_data.csv", "test_data.csv", "purchase_data.csv")
+#nmf = mf(file_path, "train_data.csv", "test_data.csv", "purchase_data.csv")
+nmf = mf(file_path, "sample_train_data.csv", "sample_test_data.csv")
 
 for row in reader:
     args = [int(row[0]), int(row[1]), float(row[2]), float(row[3])]
-    result.write("\nrank={0}, iterations={1}, lambda_={2}, alpha={3}\n".format(*args))
+#    result.write("\nrank={0}, iterations={1}, lambda_={2}, alpha={3}\n".format(*args))
     model = nmf.train(*args)
     pred = nmf.predict(model)
     nmf.evaluate(pred, result)
+    # nmf.output_predict(pred)
 
 parameters.close()
 result.close()

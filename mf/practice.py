ee82ea9f092496ec242959f38365284eba413e9d
snowery <snowery.xu@gmail.com>
2014-04-30 02:34:00 +0000

    result with complete data training model

--- mf/practice.py
+++ mf/practice.py
@@ -2,7 +2,7 @@ __author__ = 'lan'
 from MatrixFactorization import mf
 import csv
 
-file_path = "/home/ubuntu/spala/mf/"
+file_path = "/mnt/spala/mf/"
 parameters = open(file_path + "parameters.csv", 'Ur')
 reader = csv.reader(parameters, delimiter=',')
 result = open(file_path + "results", 'a')
@@ -11,8 +11,12 @@ result = open(file_path + "results", 'a')
 #nmf = mf(file_path, "train_data.csv", "purchase_test_data.csv", "purchase_data.csv")
 #nmf = mf(file_path, "train_data.csv", "sample_test_data.csv")
 
+## build model from complete dataset with missing value filled
+#nmf = mf(file_path, "train_data_fill_missing.csv", "purchase_test_data.csv", "purchase_data.csv")
+nmf = mf(file_path, "train_data_fill_missing.csv", "sample_test_data.csv")
+
 ## build model from train dataset
-nmf = mf(file_path, "purchase_train_data_fill_missing.csv", "purchase_test_data.csv", "purchase_data.csv")
+#nmf = mf(file_path, "purchase_train_data_fill_missing.csv", "purchase_test_data.csv", "purchase_data.csv")
 
 ## build model from test dataset
 #nmf = mf(file_path, "sample_train_data.csv", "sample_test_data.csv")
@@ -22,13 +26,13 @@ for row in reader:
 #    args = [int(row[0]), int(row[1]), float(row[2]), float(row[3])]
 #    result.write("\nrank={0}, iterations={1}, lambda_={2}, alpha={3}\n".format(*args))
     args = [int(row[0]), int(row[1]), float(row[2])]
-    result.write("\nrank={0}, iterations={1}, lambda_={2}\n".format(*args))
+#    result.write("\nrank={0}, iterations={1}, lambda_={2}\n".format(*args))
     
     model = nmf.train(*args)
     pred = nmf.predict(model)
     
-    nmf.evaluate(pred, result)
-#    nmf.output_predict(pred)
+#    nmf.evaluate(pred, result)
+    nmf.output_predict(pred)
 
 parameters.close()
 result.close()
